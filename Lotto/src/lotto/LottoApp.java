b881bcf75bc167d26db5ae29b2f3e2d70041b8d5
m.kikmunter <M.Kikmunter@gmail.com>
2019-11-17 18:06:17 +0000

    adjust code to college standards

--- Lotto/src/lotto/LottoApp.java
+++ Lotto/src/lotto/LottoApp.java
@@ -2,18 +2,15 @@ package lotto;
 
 import java.util.Scanner;
 
-public class LottoApp {
+public class LottoApp{
 
     //initialize variables as class properties so they can be reused within different methods
-    private static final int QUESTIONS_AMOUNT = 3;
-    private static Scanner scanner = new Scanner(System.in);
-    private static Lotto lotto = new Lotto();
+    private static final int QUESTIONS_AMOUNT=3;
+    private static Scanner scanner=new Scanner(System.in);
+    private static Lotto lotto=new Lotto();
 
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int count1,count2,count3;
-        String str = "";
         String win1, win2, win3;
         int lotteryNumbers[];
         int userNumbers[][];
@@ -25,12 +22,12 @@ public class LottoApp {
         // calling compute
         lotto.compute();
         // Getting lotto game results from Lotto class
-        count1 = lotto.getCount1();
-        count2 = lotto.getCount2();
-        count3 = lotto.getCount3();
-        win1 = lotto.getWin1();
-        win2 = lotto.getWin2();
-        win3 = lotto.getWin3();
+        count1=lotto.getCount1();
+        count2=lotto.getCount2();
+        count3=lotto.getCount3();
+        win1=lotto.getWin1();
+        win2=lotto.getWin2();
+        win3=lotto.getWin3();
         userNumbers=lotto.getUserNumbers();
         lotteryNumbers=lotto.getLotteryNumbers();
 
@@ -42,35 +39,32 @@ public class LottoApp {
         System.out.println("Second set: " + count2 + win2);
         System.out.println("Third set: " + count3 + win3);
 
-        for(int i = 0; i < 3; i++ ){
-            System.out.println((i+1) + " set numbers");
-            for(int j = 0; j < 6; j++) {
-                System.out.print(userNumbers[i][j] + " ");
+        for(int i=0;i<3;i++ ){
+            System.out.println((i+1)+" set numbers");
+            for(int j=0;j<6;j++){
+                System.out.print(userNumbers[i][j]+" ");
             }
             System.out.println();
         }
         System.out.println("---------------");
         System.out.println("Lottery numbers: ");
-        for(int i = 0; i < 6; i++ ){
-
+        for(int i=0;i<6;i++){
             System.out.print(lotteryNumbers[i]+" ");
         }
-
-
     }
 
     //ask user for his/her numbers and validate them
-    private static void askUsersForTheirGuess() {
-        for (int i = 0; i < QUESTIONS_AMOUNT; i++) {
+    private static void askUsersForTheirGuess(){
+        for(int i=0;i<QUESTIONS_AMOUNT;i++){
             System.out.println("Guess " + (i + 1) + " out of " + QUESTIONS_AMOUNT + ".");
             System.out.println("Please enter SIX numbers between 1 and 40. Please separate them by ONE space.");
-            String numbers = scanner.nextLine().trim();
-            if (!lotto.validateUserInput(numbers)) {
+            String numbers=scanner.nextLine().trim();
+            if(!lotto.validateUserInput(numbers)){
                 System.out.println("Please enter valid numbers.");
-                if (i >= 0) {
+                if (i >= 0){
                     i--;
                 }
-            } else {
+            }else{
                 lotto.setUserChoice(i, numbers);
             }
         }
